b555912c82833c5f7c30932d14be705d366c5ec4
alyssonalves1993 <33600439+alyssonalves1993@users.noreply.github.com>
2018-11-18 16:09:05 -0300

    Classe Basica produto

---
+++ src/main/java/br/com/unibratec/assistencia/model/entity/Produto.java
@@ -0,0 +1,60 @@
+package br.com.unibratec.assistencia.model.entity;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Produto implements IEntidade, Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="s_codigo_produto")
+	@SequenceGenerator(name="s_codigo_produto", 
+					   sequenceName="sequence_produto", 
+					   initialValue=1000,
+					   allocationSize = 1)
+	@Column(name="codigo_produto")
+	private Integer id;
+	public Integer getChavePrimaria() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+	public Integer getId() {
+	return id;
+	}
+	@Column(name="nome_produto", length=50)
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Column(name="descricao_produto", length=150)
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+
+}
