f422e78c639cc3e00f12c28d07260b0b32a1f2e1
alyssonalves1993 <alyssonalves1993@hotmail.com>
2019-06-02 01:13:38 -0300

    Incluindo outras classes no SuiteAll

--- test/java/br/com/unibratec/assistencia/modelo/dao/teste/SuiteAll.java
+++ test/java/br/com/unibratec/assistencia/modelo/dao/teste/SuiteAll.java
@@ -5,7 +5,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({SuiteModuloClienteEndereco.class,SuiteModuloServico.class })
+@SuiteClasses({SuiteModuloClienteEndereco.class,SuiteModuloServico.class,ProdutoTest.class,ProdutoDAOTest.class,OrdemServicoTest.class,OrdemServicoDAOTest.class})
 public class SuiteAll {
-
 }
