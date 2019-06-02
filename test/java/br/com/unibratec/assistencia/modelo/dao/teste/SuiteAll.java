633610607ffdfba8b133e261b9910782f251402c
alyssonalves1993 <alyssonalves1993@hotmail.com>
2019-06-01 23:37:49 -0300

    incluido suite all

--- test/java/br/com/unibratec/assistencia/modelo/dao/teste/SuiteAll.java
+++ test/java/br/com/unibratec/assistencia/modelo/dao/teste/SuiteAll.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ServicoDAOtest.class,ServicoTest.class })
-public class SuiteModuloServico{
+@SuiteClasses({SuiteModuloClienteEndereco.class,SuiteModuloServico.class })
+public class SuiteAll {
 
 }
