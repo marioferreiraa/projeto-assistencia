a57b9f5af4b1ba6ccac1e2cd2e090e3f7da6a5cf
Mario Bross <mariosilvaosbourne@gmail.com>
2019-06-04 06:41:01 -0300

    Alteracoes

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -73,32 +73,35 @@ $(function(){
 		return true;   
 	}
 	
-	$(".cep").on('blur',function() {
-		console.log("tentando...");
-        var cep = $(this).val().replace(/\D/g, '');
-        if (cep != "") {
-            var validacep = /^[0-9]{8}$/;
-            if(validacep.test(cep)) {
-                $(".rua").val("...");
-                $(".bairro").val("...");
-                $(".cidade").val("...");
-                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
-                    if (!("erro" in dados)) {
-                        $(".rua").val(dados.logradouro);
-                        $(".bairro").val(dados.bairro);
-                        $(".cidade").val(dados.localidade);
-                    }else {
-                        limpa_formulário_cep();
-                        alert("CEP não encontrado.");
-                    }
-                });
-            }else {
-                limpa_formulário_cep();
-                alert("Formato de CEP inválido.");
-            }
-        }else {
-            limpa_formulário_cep();
-        }
+	$(".cep").on('keyup',function() {
+		console.log("alterou");
+		if($(this).val().length == 8){
+			console.log("tentando...");
+	        var cep = $(this).val().replace(/\D/g, '');
+	        if (cep != "") {
+	            var validacep = /^[0-9]{8}$/;
+	            if(validacep.test(cep)) {
+	                $(".rua").val("...");
+	                $(".bairro").val("...");
+	                $(".cidade").val("...");
+	                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+	                    if (!("erro" in dados)) {
+	                        $(".rua").val(dados.logradouro);
+	                        $(".bairro").val(dados.bairro);
+	                        $(".cidade").val(dados.localidade);
+	                    }else {
+	                        limpa_formulário_cep();
+	                        alert("CEP não encontrado.");
+	                    }
+	                });
+	            }else {
+	                limpa_formulário_cep();
+	                alert("Formato de CEP inválido.");
+	            }
+	        }else {
+	            limpa_formulário_cep();
+	        }
+		}
     });
 	
 	function limpa_formulário_cep() {
--- test/java/br/com/unibratec/assistencia/modelo/dao/teste/TestClienteSelenium.java
+++ test/java/br/com/unibratec/assistencia/modelo/dao/teste/TestClienteSelenium.java
@@ -1,5 +1,11 @@
 package br.com.unibratec.assistencia.modelo.dao.teste;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +15,7 @@ import org.openqa.selenium.support.ui.Select;
 public class TestClienteSelenium {
 	
 	@Test
-	public void testarInserirCliente() {
+	public void testarInserirCliente() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://localhost:8080/assistencia/cadastrar-cliente.xhtml");
 		driver.findElement(By.id("formCliente:nomeCliente")).sendKeys("Mario Ferreira");	
@@ -17,6 +23,27 @@ public class TestClienteSelenium {
 		driver.findElement(By.id("formCliente:telCliente")).sendKeys("81999998999");
 		driver.findElement(By.id("formCliente:emailCliente")).sendKeys("mario@aa.aa");
 		new Select(driver.findElement(By.id("formCliente:selectSexo"))).selectByIndex(1);
+		driver.findElement(By.id("formCliente:cep")).sendKeys("51320470");
+		Thread.sleep(2000);
+		if(driver.findElement(By.id("formCliente:rua")).getAttribute("value").equals(null)) {
+			driver.findElement(By.id("formCliente:rua")).sendKeys("Rua Raposo Tavares");
+			driver.findElement(By.id("formCliente:bairro")).sendKeys("COHAB");
+			driver.findElement(By.id("formCliente:cidade")).sendKeys("Recife");
+		}
+		driver.findElement(By.id("formCliente:complemento")).sendKeys("Bloco 20");
+		driver.findElement(By.id("formCliente:numero")).sendKeys("175");
+		
+		driver.findElement(By.id("formCliente:btnSalvar")).click();
+	}
+	
+	@Test
+	public void testApiCep() throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/assistencia/cadastrar-cliente.xhtml");
+		driver.findElement(By.id("formCliente:cep")).sendKeys("51320470");
+		Thread.sleep(3000);
+		String rua = driver.findElement(By.id("formCliente:rua")).getAttribute("value");
+		assertEquals("Rua Raposo Tavares", rua);
 	}
 	
 
